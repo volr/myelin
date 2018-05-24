3cd2f4c2c95703cdc88fde738a94c56aa74e274b
Christian Pehle <christian.pehle@gmail.com>
2018-05-24 13:54:42 +0200

    Add additional assembler test

--- test/Myelin/DLS/PPU/AssemblerSpec.hs
+++ test/Myelin/DLS/PPU/AssemblerSpec.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Myelin.DLS.PPU.AssemblerSpec where
 
 import Myelin.DLS.PPU.Assembler
 import Data.Word
 import Data.Monoid
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString as B
+import Data.ByteString.Builder
 
 import Test.Hspec
 
@@ -17,10 +21,17 @@ asm =
       add R1 R2 R3
     ]
 
+
 bin :: [Word32]
 bin = map encode asm
+
+str :: ByteString
+str = toLazyByteString $ assembleInstructions asm
+
 
 spec = do
     describe "PPU Assembler" $ do
         it "can assemble ppu instructions" $ do        
             bin `shouldBe` [42076191,1115885663]
+        it "can assemble ppu instructions into bytestrings" $ do
+            str `shouldBe` "addc 0, 1, 2\nadd 1, 2, 3\n"
